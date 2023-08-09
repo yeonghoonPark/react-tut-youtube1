7a7cb5c287bbb138dd33a81c1b01c7b6cbf1869c
yeonghoon Park <huklmnj2@naver.com>
2023-08-09 23:46:28 +0900

    [feature] edited css & app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
+import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
 const queryClient = new QueryClient();
@@ -9,10 +10,12 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </RecoilRoot>
     </ThemeProvider>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "./pages/Root";
-import Home from "./pages/Home";
+import RootPage from "./pages/RootPage";
+import HomePage from "./pages/HomePage";
+import SearchedPage from "./pages/SearchedPage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        children: [
-            {
-                index: true,
-                element: <Home />,
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <RootPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/videos/:search",
+        element: <SearchedPage />,
+      },
+    ],
+  },
 ]);
 
 export default router;
